733ecba69ef19a890160dd903853d3f8ae3e95c8
Jeff Keen <jeff@jeffreykeen.com>
2010-08-05 22:35:01 -0500

    Bye bye SQLite

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rack'
 gem 'activerecord', :require => "active_record"
 gem 'sinatra-activerecord', :git => "git://github.com/dancroak/sinatra-activerecord"
 gem 'sqlite3-ruby'
+gem 'yaml_db', :git => "git://github.com/ludicast/yaml_db"
+gem 'mysql'
 
 group :development do
   gem 'rack-test'
--- environment.rb
+++ environment.rb
@@ -15,11 +15,15 @@ require "sinatra/subdomain"
 require 'erb'
 require 'rack/request'
 require 'active_record'
+require 'active_support'
 require 'sinatra/activerecord'
 require 'sqlite3'
+require 'yaml_db'
+require 'mysql'
 
 # Database setup.
-set :database, "sqlite3:///db/production.db"
+ActiveRecord::Base.establish_connection(YAML::load(File.open('db/config.yml'))["production"])   
 
 # Load Models
 require 'models'
+
